9f2f0fbd7048ecb4a7d2a1b6fc1a31251ba4a4e5
Abhilash Yadav <yadavabhilash2003@gmail.com>
2025-06-28 19:16:49 +0530

    front-end config

--- src/main/java/com/salesSavvy/config/SecurityConfig.java
+++ src/main/java/com/salesSavvy/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*")); 
+        configuration.setAllowedOrigins(List.of("https://gaming-needs.netlify.app")); 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
